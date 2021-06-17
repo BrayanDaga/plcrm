19de38e94502c8023fa057455652b9afa5065d16
giancarlo <gconyas@promolider.org>
2021-06-17 08:51:37 -0500

    db change

---
+++ database/migrations/2021_06_16_151319_create_table_account_type_by_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableAccountTypeByUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_account_type_by_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user_membreship');
+            $table->bigInteger('id_account_type');
+            $table->double('prince', 10, 2);
+            $table->integer('igv');
+            $table->double('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_account_type_by_user');
+    }
+}
