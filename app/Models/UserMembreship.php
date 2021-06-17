ad80f2eabdfa4b4034a5ff4025ac3510f2568b9c
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-17 16:19:54 -0500

    feat: agregar modelos

--- app/Models/UserMembreship.php
+++ app/Models/UserMembreship.php
@@ -6,9 +6,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class UserMembreship extends Model
 {
     use HasFactory;
     protected $table = "user_membreships";
+
+    public function country(): HasOne
+    {
+        return $this->hasOne(Country::class);
+    }
+
+    public function accountType(): HasOne
+    {
+        return $this->hasOne(AccountType::class);
+    }
+
+    public function documentType(): HasOne
+    {
+        return $this->hasOne(DocumentType::class);
+    }
+
 }
