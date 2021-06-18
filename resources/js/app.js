39d6b2b33dd2e325fdabe471c1aa4a7c1a2b0825
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-17 19:33:38 -0500

    feat: agregar componente BinaryBranch

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,19 +2,20 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-// Vue.component('binary-branch', require('./components/index.vue').default);
-Vue.component('user-membreship-register', require('./components/user-membreship/Register.vue').default);
+Vue.component('binary-branch', require('./components/binary-branch/BinaryBranch').default);
+Vue.component(
+    'user-membreship-register',
+    require('./components/user-membreship/Register.vue').default
+);
 
 import moment from 'moment';
 
-Vue.filter('formatDate', function(value) {
+Vue.filter('formatDate', function (value) {
     if (value) {
-        return moment(String(value))
-            .locale('es')
-            .format('D MMM YYYY');
+        return moment(String(value)).locale('es').format('D MMM YYYY');
     }
 });
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
 });
---
+++ resources/js/components/binary-branch/BinaryBranch.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div>Aqui ira el branch</div>
+        <div class="row" id="basic-table">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Mis Directos</h4>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            En la siguiente tabla encontrara los datos de sus directivos
+                        </p>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Pierna Afiliacion</th>
+                                    <th>Patrocinador Binario</th>
+                                    <th>Fecha de inscripcion</th>
+                                    <th>Fecha de Vencimiento</th>
+                                    <th>Activo</th>
+                                    <th>Calificado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    v-for="tempUsers in this.usersMembreship"
+                                    v-bind:key="tempUsers.id"
+                                >
+                                    <td>{{ tempUsers.name }}</td>
+                                    <td>2</td>
+                                    <td>{{ tempUsers.referrer_sponsor }}</td>
+                                    <td>{{ tempUsers.created_at | formatDate }}</td>
+                                    <td>5</td>
+                                    <td>6</td>
+                                    <td>7</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            filter: {
+                name: 'name',
+                created_at: 'created_at',
+            },
+            usersMembreship: [],
+        };
+    },
+    mounted() {
+        const url = '/api/usersMembreship/list';
+        axios
+            .get(url, {
+                params: {
+                    sName: 'name',
+                },
+            })
+            .then((response) => {
+                this.usersMembreship = response.data;
+                console.log(response.data);
+            });
+    },
+    name: 'BinaryBranch',
+};
+</script>
+
+<style scoped></style>
