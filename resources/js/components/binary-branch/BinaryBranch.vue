4adb3e89c2e0f75100b7af681d6b3887a7cbaf8d
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-18 14:50:11 -0500

    feat: agregar spinner y loading en las peticiones

--- resources/js/components/binary-branch/BinaryBranch.vue
+++ resources/js/components/binary-branch/BinaryBranch.vue
@@ -1,113 +1,125 @@
 <template>
     <div>
-        <div>Aqui ira el branch</div>
-        <div class="row" id="basic-table">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Mis Directos</h4>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            En la siguiente tabla encontrara los datos de sus directivos
-                        </p>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Posicion</th>
-                                    <th>Patrocinador</th>
-                                    <th>Tipo de Documento</th>
-                                    <th>Tipo de Cuenta</th>
-                                    <th>Telefono</th>
-                                    <th>Email</th>
-                                    <th>Pais</th>
-                                    <th>Fecha de inscripcion</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr
-                                    v-for="tempUsers in this.usersMembreship"
-                                    v-bind:key="tempUsers.id"
-                                >
-                                    <td>
-                                        <p style="width: 220px">
-                                            {{ tempUsers.name }}
-                                        </p>
-                                    </td>
-
-                                    <td>2</td>
-                                    <td>{{ tempUsers.referrer_sponsor }}</td>
-                                    <td>{{ tempUsers.document_type.document }}</td>
-                                    <td>{{ tempUsers.account_type.account }}</td>
-                                    <td>{{ tempUsers.phone }}</td>
-                                    <td>{{ tempUsers.email }}</td>
-                                    <td>{{ tempUsers.country.name }}</td>
-                                    <td>{{ tempUsers.created_at | formatDate }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <!-- Separated Pagination starts -->
-                    <div class="col-md-6 col-sm-12">
-                        <div class="card">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-center mt-2">
-                                    <li
-                                        v-if="pagination.current_page > 1"
-                                        class="page-item prev-item"
+        <custom-spinner v-if="initialLoading"></custom-spinner>
+        <template v-if="!initialLoading">
+            <div>Aqui ira el branch</div>
+            <div class="row" id="basic-table">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Mis Directos</h4>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                En la siguiente tabla encontrara los datos de sus directivos
+                            </p>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Posicion</th>
+                                        <th>Patrocinador</th>
+                                        <th>Tipo de Documento</th>
+                                        <th>Tipo de Cuenta</th>
+                                        <th>Telefono</th>
+                                        <th>Email</th>
+                                        <th>Pais</th>
+                                        <th>Fecha de inscripcion</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr
+                                        v-for="tempUsers in this.usersMembreship"
+                                        v-bind:key="tempUsers.id"
                                     >
-                                        <a
-                                            href="#"
-                                            @click.prevent="changePage(pagination.current_page - 1)"
-                                            class="page-link"
-                                        ></a>
-                                    </li>
-                                    <template v-for="page in pagesNumber">
+                                        <td>
+                                            <p style="width: 220px">
+                                                {{ tempUsers.name }}
+                                            </p>
+                                        </td>
+
+                                        <td>2</td>
+                                        <td>{{ tempUsers.referrer_sponsor }}</td>
+                                        <td>{{ tempUsers.document_type.document }}</td>
+                                        <td>{{ tempUsers.account_type.account }}</td>
+                                        <td>{{ tempUsers.phone }}</td>
+                                        <td>{{ tempUsers.email }}</td>
+                                        <td>{{ tempUsers.country.name }}</td>
+                                        <td>{{ tempUsers.created_at | formatDate }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <!-- Separated Pagination starts -->
+                        <div class="col-md-6 col-sm-12">
+                            <div class="card">
+                                <nav aria-label="Page navigation example">
+                                    <ul class="pagination justify-content-center mt-2">
                                         <li
-                                            class="page-item"
-                                            v-bind:class="[page === isActive && 'active']"
+                                            v-if="pagination.current_page > 1"
+                                            class="page-item prev-item"
                                         >
                                             <a
                                                 href="#"
+                                                @click.prevent="
+                                                    changePage(pagination.current_page - 1)
+                                                "
                                                 class="page-link"
-                                                @click.prevent="changePage(page)"
-                                            >
-                                                {{ page }}
-                                            </a>
+                                            ></a>
                                         </li>
-                                    </template>
-                                    <li
-                                        v-if="pagination.current_page < pagination.last_page"
-                                        class="page-item next-item"
-                                    >
-                                        <a
-                                            href="#"
-                                            @click.prevent="changePage(pagination.current_page + 1)"
-                                            class="page-link"
-                                        ></a>
-                                    </li>
-                                </ul>
-                            </nav>
+                                        <template v-for="page in pagesNumber">
+                                            <li
+                                                class="page-item"
+                                                v-bind:class="[page === isActive && 'active']"
+                                            >
+                                                <a
+                                                    href="#"
+                                                    class="page-link"
+                                                    @click.prevent="changePage(page)"
+                                                >
+                                                    {{ page }}
+                                                </a>
+                                            </li>
+                                        </template>
+                                        <li
+                                            v-if="pagination.current_page < pagination.last_page"
+                                            class="page-item next-item"
+                                        >
+                                            <a
+                                                href="#"
+                                                @click.prevent="
+                                                    changePage(pagination.current_page + 1)
+                                                "
+                                                class="page-link"
+                                            ></a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
                         </div>
                     </div>
+                    <!-- Separated Pagination ends -->
                 </div>
-                <!-- Separated Pagination ends -->
             </div>
-        </div>
+        </template>
     </div>
 </template>
 
 <script>
+import apiUserMembreship from '../../api/api.user-membreship';
+import CustomSpinner from '../custom-spinner/CustomSpinner';
+
 export default {
+    components: { CustomSpinner },
     created() {
         this.getUsersMembreship();
     },
     data() {
         return {
+            initialLoading: true,
             filter: {
                 name: 'name',
                 created_at: 'created_at',
@@ -152,11 +164,10 @@ export default {
     },
     methods: {
         getUsersMembreship: function (page) {
-            const url = `/api/usersMembreship/list?page=${page}`;
-            axios.get(url).then((response) => {
-                this.usersMembreship = response.data.result.data;
-                this.pagination = response.data.pagination;
-                console.log(response.data);
+            apiUserMembreship.list(page).then((response) => {
+                this.usersMembreship = response.result.data;
+                this.pagination = response.pagination;
+                this.initialLoading = false;
             });
         },
         changePage: function (page) {
