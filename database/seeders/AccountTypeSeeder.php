a7baabdbd46c367b6c0c1f4d8fd8a920210e2b44
giancarlo <gconyas@promolider.org>
2021-06-24 11:32:15 -0500

    ajustes de seed

--- database/seeders/AccountTypeSeeder.php
+++ database/seeders/AccountTypeSeeder.php
@@ -7,19 +7,14 @@ use Illuminate\Support\Facades\DB;
 
 class AccountTypeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        $accounts = array('School', 'Academy', 'University', 'Invited');
-        //
+        $accounts = array('Admin', 'School', 'Academy', 'University', 'Invited');
 
-        foreach ($accounts as $account){
+        foreach ($accounts as $account) {
             DB::table('account_type')->insert([
                 'account' => $account,
+                'status' => $account,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
