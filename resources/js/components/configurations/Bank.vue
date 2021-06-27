1a91b91d897e45773e292b228d8b45239630e27c
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-27 17:39:18 -0500

    feat: agregar formulario y tabla

--- resources/js/components/configurations/Bank.vue
+++ resources/js/components/configurations/Bank.vue
@@ -1,5 +1,57 @@
 <template>
     <div>
+        <section id="basic-horizontal-layouts">
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">{{ editMode ? 'Edit Bank' : 'Add Bank' }}</h4>
+                        </div>
+                        <div class="card-body">
+                            <form class="form form-horizontal">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="form-group row">
+                                            <div class="col-sm-3 col-form-label">
+                                                <label for="bank-name">
+                                                    Bank Name
+                                                </label>
+                                            </div>
+                                            <div class="col-sm-9">
+                                                <input
+                                                    v-model="form.name"
+                                                    type="text"
+                                                    id="bank-name"
+                                                    class="form-control"
+                                                    name="name"
+                                                    placeholder="Bank Name"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-9 offset-sm-3">
+                                        <button
+                                            @click.prevent="submit"
+                                            type="reset"
+                                            class="btn btn-primary mr-1"
+                                        >
+                                            {{ editMode ? 'Edit' : 'Add' }}
+                                        </button>
+                                        <button
+                                            @click.prevent="resetForm"
+                                            type="reset"
+                                            class="btn btn-outline-secondary"
+                                        >
+                                            Reset
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <div class="row" id="basic-table">
             <div class="col-12">
                 <div class="card">
@@ -7,7 +59,9 @@
                         <h4 class="card-title">BANK</h4>
                     </div>
                     <div class="card-body">
-                        <div class="card-text"></div>
+                        <div class="card-text">
+                            This table lists all the Promolider banks
+                        </div>
                     </div>
 
                     <div class="table-responsive">
@@ -22,8 +76,30 @@
                             <tbody>
                                 <tr>
                                     <td>1</td>
-                                    <td>a</td>
-                                    <td>as</td>
+                                    <td style="width: 220px;">Interbank</td>
+                                    <td>
+                                        <div class="demo-inline-spacing">
+                                            <button
+                                                type="button"
+                                                class="btn btn-outline-secondary round"
+                                                @click.prevent="editBank"
+                                            >
+                                                Edit
+                                            </button>
+                                            <button
+                                                type="button"
+                                                class="btn btn-outline-danger round"
+                                            >
+                                                Delete
+                                            </button>
+                                            <button
+                                                type="button"
+                                                class="btn btn-outline-info round"
+                                            >
+                                                Detail
+                                            </button>
+                                        </div>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -35,7 +111,28 @@
 </template>
 
 <script>
+const formBank = {
+    name: ''
+};
 export default {
+    data() {
+        return {
+            form: { ...formBank },
+            editMode: false
+        };
+    },
+    methods: {
+        editBank() {
+            this.editMode = true;
+        },
+        resetForm() {
+            this.form = { ...formBank };
+            this.editMode = false;
+        },
+        submit() {
+            this.resetForm();
+        }
+    },
     name: 'Bank'
 };
 </script>
