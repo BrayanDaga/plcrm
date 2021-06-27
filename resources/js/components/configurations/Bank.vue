d6e92fe2338ab6f2911160725d4562ca8bff5b0f
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-27 16:42:36 -0500

    feat: agregar formulario y tabla

--- resources/js/components/configurations/Bank.vue
+++ resources/js/components/configurations/Bank.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-        Desde Vue
+        <div class="row" id="basic-table">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">BANK</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text"></div>
+                    </div>
+
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Nro</th>
+                                    <th>Name</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>a</td>
+                                    <td>as</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
