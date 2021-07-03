71799ceced5000b1328e4bebe2bccc71516636dc
giancarlo <gconyas@promolider.org>
2021-07-03 08:45:46 -0500

    ajustes app

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,12 @@ Vue.filter('formatDate', function(value) {
   }
 });
 
-<<<<<<< HEAD
+/** Start components Config */
+Vue.component('bank', require('./components/config/bank/Bank').default);
+Vue.component('advertisement', require('./components/config/advertisement/Advertisement').default);
+Vue.component('payment-method', require('./components/config/payment-method/PaymentMethod').default);
+/** End components Config */
+
 Vue.component('binary-branch', require('./components/binary-branch/BinaryBranch').default);
 Vue.component('user-membreship-list', require('./components/user-membreship/List').default);
 Vue.component('user-membreship-register', require('./components/user-membreship/Register.vue').default);
@@ -20,23 +25,6 @@ Vue.component('user-membreship-register', require('./components/user-membreship/
 // Section Config
 // User Request
 Vue.component('user-request', require('./components/config/UserRequest.vue').default);
-=======
-/** Start components Config */
-Vue.component('bank', require('./components/config/bank/Bank').default);
-Vue.component('advertisement', require('./components/config/advertisement/Advertisement').default);
-Vue.component(
-  'payment-method',
-  require('./components/config/payment-method/PaymentMethod').default
-);
-/** End components Config */
-
-Vue.component('binary-branch', require('./components/binary-branch/BinaryBranch').default);
-Vue.component('user-membreship-list', require('./components/user-membreship/List').default);
-Vue.component(
-  'user-membreship-register',
-  require('./components/user-membreship/Register.vue').default
-);
->>>>>>> e2b61732bd9569b33a464ad165b9509c219e4f5a
 
 const app = new Vue({
   el: '#app'
