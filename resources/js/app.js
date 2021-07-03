ea27ecd39ba210c166760e1f05cdc343fd592574
giancarlo <gconyas@promolider.org>
2021-07-03 10:57:22 -0500

    ajustes files

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,15 +16,12 @@ Vue.filter('formatDate', function(value) {
 Vue.component('bank', require('./components/config/bank/Bank').default);
 Vue.component('advertisement', require('./components/config/advertisement/Advertisement').default);
 Vue.component('payment-method', require('./components/config/payment-method/PaymentMethod').default);
+Vue.component('user-request', require('./components/config/UserRequest.vue').default);
 /** End components Config */
 
 Vue.component('binary-branch', require('./components/binary-branch/BinaryBranch').default);
 Vue.component('user-membreship-list', require('./components/user-membreship/List').default);
 Vue.component('user-membreship-register', require('./components/user-membreship/Register.vue').default);
-
-// Section Config
-// User Request
-Vue.component('user-request', require('./components/config/UserRequest.vue').default);
 
 const app = new Vue({
   el: '#app'
