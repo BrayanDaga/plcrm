211d45750f3d6f820060a2615db4f12b0418abd1
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-07-14 14:16:20 -0500

    evitando-llamar-datatable-en-todos-los-views

--- resources/views/content/config/account-type.blade.php
+++ resources/views/content/config/account-type.blade.php
@@ -7,6 +7,10 @@
 @section('vendor-style')
     <!-- vendor css files -->
     <link rel="stylesheet" href="{{ asset(mix('vendors/css/extensions/toastr.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/dataTables.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/responsive.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap4.min.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap4.min.css')) }}">
 @endsection
 @section('page-style')
     <!-- Page css files -->
@@ -25,7 +29,7 @@
     <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
     <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.responsive.min.js')) }}"></script>
     <script src="{{ asset(mix('vendors/js/tables/datatable/responsive.bootstrap4.js')) }}"></script>
-    <@endsection
+    @endsection
 @section('page-script')
     <!-- Page js files -->
     <script src="{{ asset(mix('js/scripts/extensions/ext-component-toastr.js')) }}"></script>
