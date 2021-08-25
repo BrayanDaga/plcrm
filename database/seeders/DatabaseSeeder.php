41be689e65dfbf310789f77a5cb5da82f416f7b0
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-08-25 11:47:28 -0500

    Usando-Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,13 +19,13 @@ class DatabaseSeeder extends Seeder
         $this->call(DocumentTypeSeeder::class);
         $this->call(AccountTypeSeeder::class);
         $this->call(PaymentMethodSeeder::class);
-        // $this->call(BankSeeder::class);
+        $this->call(BankSeeder::class);
         // UserMembreship::factory()->create(); // se reduce la cantida de datos de pruebas
-        // $this->call(UserMembreshipSeeder::class);
+         $this->call(UserMembreshipSeeder::class);
         // $this->call(PaymentSeeder::class);
-        // $this->call(AdvertisementSeeder::class);
-        // $this->call(ClassifiedSeeder::class);
-        // $this->call(WalletSeeder::class);
+        $this->call(AdvertisementSeeder::class);
+        $this->call(ClassifiedSeeder::class);
+        $this->call(WalletSeeder::class);
         /*$this->call(ClassifiedSeeder::class);*/
     }
 }
