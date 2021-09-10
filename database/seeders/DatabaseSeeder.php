0da05e539ef29547ff64ed065368a6f674aa10c6
Brayan Vilchez Daga <brayandaga5@gmail.com>
2021-09-10 17:32:34 -0500

    Creando-function-myqsl-en-seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\FunctionsSeeder;
 use App\Models\Classified;
 use App\Models\UserMembreship;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,5 +29,27 @@ class DatabaseSeeder extends Seeder
         // $this->call(ClassifiedSeeder::class);
         $this->call(WalletSeeder::class);
         /*$this->call(ClassifiedSeeder::class);*/
+
+        /*creando function mysql para Obtener todos los nodos secundarios de un nodo
+        mas info: https://programmerclick.com/article/13791419702/   */
+        $getChildNodeFunction = "
+        CREATE FUNCTION `GET_CHILD_NODE`(rootId varchar(100))   
+        RETURNS varchar(2000)  
+        BEGIN   
+        DECLARE str varchar(2000);  
+        DECLARE cid varchar(100);   
+        SET str = '$';   
+        SET cid = rootId;   
+        WHILE cid is not null DO   
+            SET str = concat(str, ',', cid);   
+            SELECT group_concat(id) INTO cid FROM user_membreships where FIND_IN_SET(id_referrer_sponsor, cid);   
+        END WHILE;   
+        RETURN str;   
+        END
+";
+
+        DB::unprepared("DROP FUNCTION IF EXISTS GET_CHILD_NODE"); //borrando la funcion si existe
+        DB::unprepared($getChildNodeFunction); //creando la funcion
+
     }
 }
