6f1dff264b7dc889b67ddd5dc541f8aeb3de2056
Giancarlo <gcarlo1691@gmail.com>
2021-10-20 11:50:53 -0500

    implementacion de api router

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,22 @@
 <?php
 
 use App\Http\Controllers\AccountTypeController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\UserMembreshipController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => '/v1'], function(){
+    if(config('app.is_api')){
+        Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function(){
+            Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+            Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:api');
+        });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        Route::middleware(['auth:api', 'checkAuth'])->group(function(){
+
+            //Api Account Type
+            Route::group(['prefix' => '/accout-type'], function(){
+                Route::get('/{id}', [AccountTypeController::class, 'getDataBytId']);
+            });
+        });
+    }
 });
-
-
-Route::get('/account-type/get-data-id/{id}', [AccountTypeController::class, 'getDataBytId']);
-
-// Route::post('/user-membreship/create', [UserMembreshipController::class, 'create']);
