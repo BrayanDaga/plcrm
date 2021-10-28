5f200cd73014569f744591145627ce9e77d24f81
Jhonatan <7jhon2898@gmail.com>
2021-10-28 15:22:24 -0500

    api para cursos/modulos/clases

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -11,22 +11,26 @@ class CourseController extends Controller
 {
     public function show($id){
         $curso = Course::find($id);
-        $modules = Module::where('id_courses','=',$id)->get();
-        $clas = [];
-        $modulesJson=[];
-        foreach($modules as $mod){
-            $clas = Clas::where('id_modules',$mod->id)->get();
-            $modulesJson[] = [
-                'id_module' =>$mod->id,
-                'module'    => $mod->name,
-                'lessons'    => $clas
+        if($curso){
+            $modules = Module::where('id_courses','=',$id)->get();
+            $clas = [];
+            $modulesJson=[];
+            foreach($modules as $mod){
+                $clas = Clas::where('id_modules',$mod->id)->get();
+                $modulesJson[] = [
+                    'id_module' =>$mod->id,
+                    'module'    => $mod->name,
+                    'lessons'    => $clas
+                ];
+            }
+            $courseJson = [
+                'id_course' => $curso->id,
+                'course'    => $curso->title,
+                'modules'   => $modulesJson
             ];
+            return $courseJson;   
+        }else{
+            return "no existen cursos";
         }
-        $courseJson = [
-            'id_course' => $curso->id,
-            'course'    => $curso->title,
-            'modules'   => $modulesJson
-        ];
-        return $courseJson;
     }
 }
