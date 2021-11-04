bb93efe50822a82ae5dee575ab282855a99da99f
Giancarlo <gcarlo1691@gmail.com>
2021-11-04 10:13:11 -0500

    orden declaracion de rutas api

--- routes/api.php
+++ routes/api.php
@@ -9,37 +9,40 @@ use App\Http\Controllers\PropiertiesforUser;
 use Illuminate\Support\Facades\Route;
 
 //Post Store User
-Route::group(['prefix' => '/v1'], function(){
-    Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function(){
+Route::group(['prefix' => '/v1'], function () {
+    Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
         Route::post('/register', [AuthController::class, 'store']);
     });
 });
 
-Route::group(['prefix' => '/v1'], function(){
-    if(config('app.is_api')){
-        Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function(){
+Route::group(['prefix' => '/v1'], function () {
+    if (config('app.is_api')) {
+        Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
             Route::post('login', [AuthController::class, 'login'])->name('auth.login');
             Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth:api');
         });
 
-        Route::middleware(['auth:api', 'checkAuth'])->group(function(){
+        Route::middleware(['auth:api', 'checkAuth'])->group(function () {
             //Api Account Type
-            Route::group(['prefix' => '/accout-type'], function(){
+            Route::group(['prefix' => '/accout-type'], function () {
                 Route::get('/{id}', [AccountTypeController::class, 'getDataBytId']);
             });
-            Route::group(['prefix' => '/course'],function(){
-                Route::group(['prefix' => '/temary'],function(){
-                    Route::get('/get-all-class/{id}',[CourseController::class,'show']);
-                });
-            });
-            Route::group(['prefix'=> '/messages'],function(){
-                Route::get('/with/{email}',[MessageController::class,'show']);
-            });
-            Route::group(['prefix' => '/list-course'], function(){
-                Route::get('/producter/{id}', [CourseController::class,'list']);
-            });  
-            Route::get('/getattributes', [PropiertiesforUser::class,'getPropierties']);
 
+            //Api Message
+            Route::group(['prefix' => '/messages'], function () {
+                Route::get('/with/{email}', [MessageController::class, 'show']);
+            });
+
+            //Api Course
+            Route::group(['prefix' => '/course'], function () {
+                Route::get('/temary/get-all-class/{id}', [CourseController::class, 'show']);
+                Route::get('/list-course/producter/{id}', [CourseController::class, 'list']);
+            });
+
+            //Api Dashboard
+            Route::group(['prefix' => 'dashboard'], function () {
+                Route::get('/getattributes', [PropiertiesforUser::class, 'getPropierties']);
+            });
         });
     }
 });
